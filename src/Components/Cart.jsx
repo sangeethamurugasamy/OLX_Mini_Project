f5cdd14caa4a3d67a98b56145a2dd834cb7f1e0f
Divagaran <rajaadivagaran@gmail.com>
2021-09-22 22:41:11 +0530

    add cart file

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+export class Cart extends React.Component{
+    render(){
+        return <h1>Welcome to cart</h1>
+    }
+}
