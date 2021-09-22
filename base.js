8bc736fb543d956ca44bf3265fd14ce78caf9632
ranadpsingh <58307947+ranadpsingh@users.noreply.github.com>
2021-09-22 12:25:56 +0530

    Add files via upload

---
+++ base.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyD9dTqdfQhZ4hFp0saBFLFPQc4tf3B7_-I",
+  authDomain: "login-auth-337de.firebaseapp.com",
+  projectId: "login-auth-337de",
+  storageBucket: "login-auth-337de.appspot.com",
+  messagingSenderId: "343166477280",
+  appId: "1:343166477280:web:1eb48ddcf99d9a62528185",
+  measurementId: "G-7R0QJR84JS"
+});
+
+  
+  export default app;
